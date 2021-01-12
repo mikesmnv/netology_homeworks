2a3afe459a99f6749e0e8e0cbb75af005317bf7e
mikesmnv <mikhailsmv89@gmail.com>
2021-01-12 10:48:58 +0300

    ex 5

---
+++ drf-intro/simple_crud1/measurements_new/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeasurementsNewConfig(AppConfig):
+    name = 'measurements_new'
---
+++ drf-intro/simple_crud1/measurements_new/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from models import Project, Measurement
+from simple_crud1.measurements_new.serializers import ProjectSerializer, MeasurementSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class MeasurementViewSet(ModelViewSet):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
